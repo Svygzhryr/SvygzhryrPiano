723dda8bc9a1af02dbe5357661dcdd52013db1c2
Svygzhryr <pamixy@gmail.com>
2023-01-17 08:53:41 +0300

    theme saving in localStorage

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { COLORS } from '../global/constants';
 import { useState, useRef } from 'react';
 import '../css/ui.scss'
@@ -8,6 +8,10 @@ export default function UI({...props}) {
 
   const [theme, setTheme] = useState('black');
 
+  useEffect(() => {
+    document.documentElement.setAttribute('color-scheme', localStorage.getItem('theme') ?? 'default')
+  }, [theme])
+
   function handleShowText() {
     localStorage.setItem('text', !props.showText)
     return props.setShowText(!props.showText)
@@ -15,44 +19,30 @@ export default function UI({...props}) {
   
   // смена цветовой схемы
   const themeChange = (e) => {
-    const root = document.querySelector(':root');
-    let rootStyles = getComputedStyle(root);
     const button = e.target;
     switch (true) {
         default: return null;
         case button.classList.contains('theme_1'): {
-            root.style.setProperty('--primary_background', COLORS.black_background)
-            root.style.setProperty('--primary_button', COLORS.black_button)
-            root.style.setProperty('--primary_sharp', COLORS.black_sharp)
-            root.style.setProperty('--primary_button_active', COLORS.black_button_active)
-            root.style.setProperty('--primary_sharp_active', COLORS.black_sharp_active)
+            setTheme('default');
+            localStorage.setItem('theme', 'default')
             break
         }
 
         case button.classList.contains('theme_2'): {
-            root.style.setProperty('--primary_background', COLORS.purplish_background)
-            root.style.setProperty('--primary_button', COLORS.purplish_button)
-            root.style.setProperty('--primary_sharp', COLORS.purplish_sharp)
-            root.style.setProperty('--primary_button_active', COLORS.purplish_button_active)
-            root.style.setProperty('--primary_sharp_active', COLORS.purplish_sharp_active)
+            setTheme('purple');     
+            localStorage.setItem('theme', 'purple')
             break
         }
 
         case button.classList.contains('theme_3'): {
-            root.style.setProperty('--primary_background', COLORS.reddish_background)
-            root.style.setProperty('--primary_button', COLORS.reddish_button)
-            root.style.setProperty('--primary_sharp', COLORS.reddish_sharp)
-            root.style.setProperty('--primary_button_active', COLORS.reddish_button_active)
-            root.style.setProperty('--primary_sharp_active', COLORS.reddish_sharp_active)
+            setTheme('red');
+            localStorage.setItem('theme', 'red')
             break
         }
 
         case button.classList.contains('theme_4'): {
-            root.style.setProperty('--primary_background', COLORS.bluish_background)
-            root.style.setProperty('--primary_button', COLORS.bluish_button)
-            root.style.setProperty('--primary_sharp', COLORS.bluish_sharp)
-            root.style.setProperty('--primary_button_active', COLORS.bluish_button_active)
-            root.style.setProperty('--primary_sharp_active', COLORS.bluish_sharp_active)
+            setTheme('blue');
+            localStorage.setItem('theme', 'blue')
             break
         }
     }
