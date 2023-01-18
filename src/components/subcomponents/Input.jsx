6a8ed79d648d9fdc0546d44a9ba5bc0159f73276
Svygzhryr <pamixy@gmail.com>
2023-01-18 14:57:45 +0300

    reverb slider

--- src/components/subcomponents/Input.jsx
+++ src/components/subcomponents/Input.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 export default function Input({...props}) {
   return (
-    <div className='slider_wrapper'>
-        <input step={1} min={-30} max={20} value={props.volume} onChange={(e) => {props.changeVolume(e.target.value)}} className='range' type='range'/>
+    <div >
     </div>
   )
 }
