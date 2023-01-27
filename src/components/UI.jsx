24c225c089451560a27911f996894ad37b68f599
Svygzhryr <pamixy@gmail.com>
2023-01-27 14:37:22 +0300

    envelope

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -69,26 +69,28 @@ export default function UI({...props}) {
           </div>
         </div>
         <div className="extra-controls">
-          <div className="controls-envelope"></div>
+        <div className="envelope">
+            
+          </div>
           <div className="controls-fx">
-          <div className="effect-slider reverb">
-            <input  step={1} min={0.001} max={51} value={props.reverb} onChange={(e) => {props.changeReverb(e.target.value)}} className='range' type='range'/>
-          </div>
-          {/* <div className="effect-slider delay-duration">
-            <input  step={1} min={1} max={24} value={props.delayDuration} onChange={(e) => {props.changeDelayDuration(e.target.value)}}  className='range' type='range'/>
-          </div>
-          <div className="effect-slider delay-feedback">
-            <input  step={0.1} min={0} max={1} value={props.delayFeedback} onChange={(e) => {props.changeDelayFeedBack(e.target.value)}}  className='range' type='range'/>
-          </div> */}
-          <div className="octave">
-            <button onClick={() => {props.setDetune(props.detune < -1200 ? props.detune : props.detune - 1200)}} className="octave-down">-</button>
-            <h1 className="detune-number">
-              {(props.detune/1200)}
-            </h1>
-            <button onClick={() => {props.setDetune(props.detune > 2400 ? props.detune : props.detune + 1200)}} className="octave-up">+</button>
-          </div>
-          </div>
-          <div className="show-controls"><AiOutlineRight className='show-controls-arrow'/></div>
+            <div className="effect-slider reverb">
+              <input  step={1} min={0.001} max={51} value={props.reverb} onChange={(e) => {props.changeReverb(e.target.value)}} className='range' type='range'/>
+            </div>
+            {/* <div className="effect-slider delay-duration">
+              <input  step={1} min={1} max={24} value={props.delayDuration} onChange={(e) => {props.changeDelayDuration(e.target.value)}}  className='range' type='range'/>
+            </div>
+            <div className="effect-slider delay-feedback">
+              <input  step={0.1} min={0} max={1} value={props.delayFeedback} onChange={(e) => {props.changeDelayFeedBack(e.target.value)}}  className='range' type='range'/>
+            </div> */}
+            <div className="octave">
+              <button onClick={() => {props.setDetune(props.detune < -1200 ? props.detune : props.detune - 1200)}} className="octave-down">-</button>
+              <h1 className="detune-number">
+                {(props.detune/1200)}
+              </h1>
+              <button onClick={() => {props.setDetune(props.detune > 2400 ? props.detune : props.detune + 1200)}} className="octave-up">+</button>
+            </div>
+            </div>
+            <div className="show-controls"><AiOutlineRight className='show-controls-arrow'/></div>
         </div>
         <button className="instrument-switch" onClick={() => {props.setInstrument(!props.instrument)}}>{props.instrument ? <MdPiano/> : <RiSoundModuleFill/>}</button>
 
