10e078f08e7706815b05bef9bd6972aa8ced21db
Svygzhryr <pamixy@gmail.com>
2023-01-27 14:37:30 +0300

    envelope

--- src/components/subcomponents/Knob.jsx
+++ src/components/subcomponents/Knob.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default function Input({...props}) {
   return (
-    <div >
+    <div className='envelope_knob'>
+      <input type="slider" />
     </div>
   )
 }
