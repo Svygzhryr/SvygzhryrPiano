97d2b4fda2dcae8b4468dbbe484711974aa10503
Svygzhryr <pamixy@gmail.com>
2023-02-03 14:08:52 +0300

    sampler + crossbrowser fixing

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -5,7 +5,7 @@ import * as Tone from 'tone'
 import styles from '../css/piano.module.scss'
 import { UPPER_NOTES, LOWER_NOTES, KEY_TO_NOTE, NOTE_TO_KEY, COLORS} from '../global/constants'
 import UI from './UI';
-import { delay } from 'lodash';
+import { delay, now } from 'lodash';
 
 // здесь пришлось пойти на компромисс между разрывами звука и задержкой при нажатии
 // начиная со значения 0.05 задержка становится заметной, как и пердёж если ставить ниже 0.02
@@ -39,7 +39,15 @@ let monosynth = new Tone.PolySynth(Tone.MonoSynth).connect(FXreverb, FXtremolo).
 let fmsynth = new Tone.PolySynth(Tone.FMSynth).connect(FXreverb, FXtremolo).toDestination();
 let amsynth = new Tone.PolySynth(Tone.AMSynth).connect(FXreverb, FXtremolo).toDestination();
 let membranesynth = new Tone.PolySynth(Tone.MembraneSynth).connect(FXreverb, FXtremolo).toDestination();
-let sampler = new Tone.PolySynth(Tone.Sampler).connect(FXreverb, FXtremolo).toDestination();
+let sampler = new Tone.Sampler({
+    urls: {
+        A1: "A1.mp3",
+        A2: "A2.mp3",
+    },
+    baseUrl: "https://tonejs.github.io/audio/casio/",
+}).connect(FXreverb, FXtremolo).toDestination();
+
+
 
 const instruments = [synth, monosynth, fmsynth, amsynth, membranesynth, sampler];
 instruments.forEach((e) => {
@@ -60,8 +68,9 @@ instruments.forEach((e) => {
         }
     })
 })
-
+Tone.start()
 let activeSynth;
+var allowed = true;
 
 export default function Piano() {
     let keyClassName;
@@ -98,6 +107,7 @@ export default function Piano() {
         const key = e.key.toLowerCase();
         const shittySharp = CSS.escape(KEY_TO_NOTE[key]);
         const button = document.querySelector(`[note=${shittySharp}]`);
+
         try {
             button.classList.contains(styles.button) ? 
             button.classList.add(styles.button_active) : 
@@ -118,15 +128,18 @@ export default function Piano() {
           
         activeSynth.volume.value = volume;
         activeSynth.triggerAttack(note);
+        console.log(note, 'played')
     }
 
     // отпускание клавиши
     // eslint-disable-next-line react-hooks/exhaustive-deps
     const handleKeyUp = (e) => {
         
+        allowed = true;
         const key = e.key.toLowerCase();
         const shittySharp = CSS.escape(KEY_TO_NOTE[key]);
-        activeSynth.triggerRelease(KEY_TO_NOTE[key]);
+        console.log(KEY_TO_NOTE[key], 'r')
+        activeSynth.triggerRelease(KEY_TO_NOTE[key])
         const button = document.querySelector(`[note=${shittySharp}]`);
         try {
             button.classList.contains(styles.button_active) ? 
@@ -286,7 +299,7 @@ export default function Piano() {
             <button onClick={(e) => {handleInstruments('fmsynth', e.target)}} className={`${styles.instrument_item}  ${switchInstrument == 'fmsynth' ? styles.instrument_active : ''}`}>FMSynth</button>
             <button onClick={(e) => {handleInstruments('amsynth', e.target)}} className={`${styles.instrument_item}  ${switchInstrument == 'amsynth' ? styles.instrument_active : ''}`}>AMSynth</button>
             <button onClick={(e) => {handleInstruments('membranesynth', e.target)}} className={`${styles.instrument_item} ${switchInstrument == 'membranesynth' ? styles.instrument_active : ''}`}>MemSynth</button>
-            <button onClick={(e) => {handleInstruments('sampler', e.target)}} className={`${styles.instrument_item} ${styles.disabled}`}>Sampler <br/> <p>(in development)</p></button>
+            <button onClick={(e) => {handleInstruments('sampler', e.target)}} className={`${styles.instrument_item} ${switchInstrument == 'sampler' ? styles.instrument_active : ''}`}>Sampler</button>
         </div>
 
         <div className={`${styles.piano_wrapper} ${styles.active} ${instrument ? '' : styles.inactive}`}>
